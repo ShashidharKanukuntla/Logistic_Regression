3ad3c561812036305047fc2e2a6615b9461bbca0
shashidhark97 <shashidharkanukuntla@gmail.com>
2020-08-29 10:37:49 +0530

    updated preprocess utils

--- preprocess_utils.py
+++ preprocess_utils.py
@@ -6,9 +6,11 @@ Created on Thu Aug 27 21:48:23 2020
 """
 import numpy as np
 
+# X should be of shape (no. of features, no. of examples)
 def normalizeData(x):
     return x/np.max(x, axis=1, keepdims=True)
 
+# X should be of shape (no. of features, no. of examples), Y should be of shape (1, no. of examples) 
 def test_train_split(X, Y, ratio):
     X_train = X[:, :int(X.shape[1]*(1-ratio))]
     X_test = X[:, int(X.shape[1]*(1-ratio)):]
